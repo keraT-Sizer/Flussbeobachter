8a30633f042a3f63371ec00b43ec101d24507ef2
Tarek Aouadhi <tarekaouadhi@gmail.com>
2020-02-12 15:28:07 +0100

    Projektdateien hinzufügen.

---
+++ Flußbeobachter/Fluß.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flußbeobachter
+{
+    class Fluß
+    {
+        public string Name { get; set; } 
+        public int Wasserstand { get; set; }
+        public Stadt StadtBeobachter_1 = new Stadt();
+        public Stadt StadtBeobachter_2 = new Stadt();
+        public Schiffe SchiffeBeobachter_1 = new Schiffe();
+        public Schiffe SchiffeBeobachter_2 = new Schiffe();
+        public Klärwerke KlärwerkeBeobachter = new Klärwerke {  Name="Strauß 1"};
+
+        public event EventHandler<EventArgs> AnhaltEvent;
+        public event EventHandler<EventArgs> WasserschutzEvent;
+        public event EventHandler<EventArgs> EinleitungStopEvent;
+        public event EventHandler<EventArgs> EinleitungSteigEvent;
+        private Random zfg = new Random();
+
+        public void WasserFluß(int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+
+                this.Wasserstand = zfg.Next(100, 10001);
+                //Wasserstand = 9300; 
+                Console.WriteLine();
+                Console.WriteLine("Die Aktuelle Wasserzustand ist: {0}  ", Wasserstand); Console.WriteLine();
+                Thread.Sleep(2000);
+
+                if (Wasserstand > 3000 &&  Wasserstand < 8000)
+                {
+                    Console.WriteLine("Alles ist normal kein Sorge....");
+                }
+
+                if (Wasserstand >= 8000)
+                {
+                    Console.WriteLine("Es ist gefährlich !! - Diese Vorsorge sind genommen: ");
+                    Console.WriteLine("==============================");
+                    AnhaltEvent(this, new EventArgs());
+                    EinleitungStopEvent(this, new EventArgs());
+                    
+                    if (Wasserstand >= 8200)
+                        
+                        WasserschutzEvent(this, new EventArgs());
+                    Console.WriteLine("==============================");
+                }
+                if (Wasserstand <= 3000)
+                {
+                     Console.WriteLine("Es ist gefährlich !! - Diese Vorsorge sind genommen: ");
+                    Console.WriteLine("==============================");
+                    EinleitungSteigEvent(this, new EventArgs());
+
+                    if (Wasserstand <= 250)
+                        AnhaltEvent(this, new EventArgs());
+                    Console.WriteLine("==============================");
+                }
+                
+            }
+                 
+        }
+        
+    }
+    class Schiffe 
+    {
+        public string Name { get; set; }
+        public void Anhalten(object o, EventArgs e)
+        {
+            if (o != null)
+                Console.WriteLine ("{0} sind eingehalten...",Name);
+        }
+    }
+    class Stadt 
+    {
+        public string Name { get; set; }
+        public void Wasserschutzwand(object o, EventArgs e)
+        {
+            if (o!=null)
+                Console.WriteLine("{0} Wasserschutzwand An..", Name);
+        }
+    } 
+    class Klärwerke 
+    {
+        public string Name { get; set; }
+        public void Einleintung1(object o, EventArgs e)
+        {
+            if (o != null)
+                Console.WriteLine("{0} Einleitung gestoppt..", Name);
+        }
+        public void Einleintung2(object o, EventArgs e)
+        {
+            if (o != null)
+                Console.WriteLine("{0} Einleitung steigt..", Name);
+        }
+    }
+}
---
+++ Flußbeobachter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flußbeobachter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fluß Rhein = new Fluß() { Name= "Rhein", StadtBeobachter_1 ={ Name = "Düsseldorf" }, StadtBeobachter_2 =  { Name = "Köln" },
+            SchiffeBeobachter_1= { Name="Rheingold"},SchiffeBeobachter_2= { Name="Lorelei"}};
+
+            Fluß Donau = new Fluß() { Name= "Donau", StadtBeobachter_1= {  Name="Ulm"}, 
+                SchiffeBeobachter_2 =  { Name = "Franz" }, SchiffeBeobachter_1 = {  Name="Xaver"} };
+
+            string e = "j";
+            
+            do
+            {
+                Console.Write("Welsche Fluß wollen sie beobachten Rhein='r' oder Donau='d': "); e = Console.ReadLine();
+                
+                switch (e)
+                {
+                    case "r":
+                        Console.Write("wie Viel Beobachten wollen sie machen: "); int anzahl = Convert.ToInt16(Console.ReadLine());
+                        Eventfunction(Rhein,anzahl);
+                        break;
+
+                    case "d":
+                        Console.Write("wie Viel Beobachten wollen sie machen: "); int anzahl2 = Convert.ToInt16(Console.ReadLine());
+                        Eventfunction(Donau,anzahl2);
+                        break;
+
+                    default:
+                        Console.WriteLine("Falsch angabe.");
+                        break;
+                }
+                Console.Write("Wollen sie noch mal Beobachten: "); e = Console.ReadLine();
+            } while (e=="j" );
+            
+            void Eventfunction(Fluß F,int anzahl)
+            {
+                F.AnhaltEvent += F.SchiffeBeobachter_1.Anhalten;
+                F.AnhaltEvent += F.SchiffeBeobachter_2.Anhalten;
+                F.WasserschutzEvent += F.StadtBeobachter_1.Wasserschutzwand;
+                F.EinleitungStopEvent += F.KlärwerkeBeobachter.Einleintung1;
+                F.EinleitungSteigEvent += F.KlärwerkeBeobachter.Einleintung2;
+                if (F==Rhein)
+                    F.WasserschutzEvent += F.StadtBeobachter_2.Wasserschutzwand;
+               
+
+                F.WasserFluß(anzahl);
+                
+            }
+           
+
+            
+        }
+    }
+    
+}
